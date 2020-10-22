16aa40a0b347cda5d3685728324f682790664ff8
liranp <liran.polak@gmail.com>
2020-10-22 16:42:22 +0300

    chore(cleanup): remove comments

--- src/index.js
+++ src/index.js
@@ -5,8 +5,3 @@ import "./scripts/docsify";
 // Styles
 
 import "./styles/main.pcss";
-// import "./styles/base.scss";
-// import "./styles/sidebar.scss";
-// import "./styles/search.scss";
-// import "./styles/markdown.scss";
-// import "./styles/code.scss";
